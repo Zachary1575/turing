964469b25ed51508d21139d91ca79b4553c84f97
Zachary <75510412+Zachary1575@users.noreply.github.com>
2025-06-25 11:16:30 -0400

    Added protobuff implementation for python to cpp serialization

---
+++ src_cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+// We need to recieve the item through in-memory or smthing.
+// This feels like a very decoupled architecture.
+
+int main() {
+    cout << "Operation start!" << endl;
+    return 0;
+}
---
+++ src_python/main.py
@@ -0,0 +1,22 @@
+# Lets try to write one thing in this file and then push out organically.
+def parse(expression) -> None:
+    print(f"Given Expression: {expression}")
+    
+    # Do some stuff with the expression
+    # ...
+    # ...
+    # ...
+
+    # Then we package it up with the serializer provided by protoc
+
+    # Then slap it into in-memory
+
+    print("Operation Done.")
+    return
+
+
+def main() -> None:
+    print("Ayyy!")
+    return
+
+main()
