a98e7494795c39fa69046532f4da0f2fb2806baf
Zachary <75510412+Zachary1575@users.noreply.github.com>
2025-06-26 21:27:30 -0400

    Added protobuf implementation

--- src_python/main.py
+++ src_python/main.py
@@ -1,13 +1,23 @@
+from protobuf import example_pb2
+
 # Lets try to write one thing in this file and then push out organically.
 def parse(expression) -> None:
     print(f"Given Expression: {expression}")
-    
+
     # Do some stuff with the expression
     # ...
     # ...
     # ...
 
     # Then we package it up with the serializer provided by protoc
+    person = example_pb2.Person()
+    person.name = "Alice"
+    person.id = 1
+    person.email = "alice@hotmail.com"
+
+    # Serialize into bytes
+    data = person.SerializeToString()
+    print(f"Byte Data: ${data}")
 
     # Then slap it into in-memory
 
